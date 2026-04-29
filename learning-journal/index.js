ec313abe3e2f3a499e9097134a895574c90cad48
dwight-foster <dwightfoster03@gmail.com>
2026-04-29 12:02:23 -0400

    finished solo project. JS was difficult getting the pages to render correctly. Not my best work

--- learning-journal/index.js
+++ learning-journal/index.js
@@ -4,9 +4,20 @@ const mediaQuery = window.matchMedia('(min-width: 1085px)');
 const blog = document.getElementById('blogs');
 const showMore = document.getElementById('show-more');
 
-document.addEventListener('click', (e) => {
-    
+mediaQuery.addEventListener('change', (e) => {
+  renderBlog(blogData, e);
 });
+
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.post) {
+        localStorage.setItem('selectedPostId', e.target.dataset.post);
+        window.location.href = 'post.html';
+
+    }
+
+});
+
+
 
 function renderBlog (blogData, e) {
     if (e.matches && window.location.pathname === "/learning-journal/index.html") {
@@ -20,8 +31,8 @@ function renderBlog (blogData, e) {
         const {title, date, text, img, id, _} = post;
         return `
                 <div class="post">
-                    <a href="#" data-post="${id}">
-                      <img class="post-img" src="${img}">
+                    <a href="post.html">
+                      <img class="post-img" src="${img}" data-post="${id}">
                     </a>
                     <div class="post-text">
                         <p class="post-date">${date}</p>
@@ -40,13 +51,29 @@ function renderBlog (blogData, e) {
     }
 }
 
-function renderPost (blogPost) {
-    
+export function renderPost (blogPost) {
+    const {title, date, text, img, id, sections} = blogPost;
+    const htmlSectionText = sections.map(function (section) {
+        const {heading, body} = section;
+        return `
+              <h1 class="section-title">${heading}</h1>
+              <p class="section-description">${body}</p>
+              `      
+    }).join('');
+    const htmlText = `
+            <div class="title-section">
+                <p class="desc-date">${date}</p>
+                <h1 class="desc-title">${title}</h1>
+                <p class="desc-text">${text}</p>
+            </div>
+            <img src=${img} class="title-img">
+            <div class="post-body">
+              ${htmlSectionText}
+            </div>`;
+    console.log(document.getElementById('post'));
+    document.getElementById('post').innerHTML = htmlText;
 
 }
 
 renderBlog(blogData, mediaQuery);
 
-mediaQuery.addEventListener('change', (e) => {
-  renderBlog(blogData, e);
-});
